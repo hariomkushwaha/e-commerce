35902967b562c9a03178e576e2659d0db62d9c61
hariomkushwaha <hari2018cs048abesit@gmail.com>
2020-06-11 15:02:37 +0530

    used styled-component to convert the style into js and converted the header and custombutton component's styling

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 
-import './custom-buttom.styles.scss';
 
-const CustomButton = ({
-  children,
-  isGoogleSignIn,
-  inverted,
-  ...otherProps
-}) => (
-  <button
-    className={`${inverted ? 'inverted' : ''} ${
-      isGoogleSignIn ? 'google-sign-in' : ''
-    } custom-button`}
-    {...otherProps}
+import { CustomButtonContainer } from './custom-button';
+
+
+const CustomButton= ({children,...props}) => (
+  <CustomButtonContainer
+    {...props}
   >
     {children}
-  </button>
+  </CustomButtonContainer>
 );
 
 export default CustomButton;
